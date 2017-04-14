3c3e4cecd3dcbb1123a93395b5feee51ede3e1f5
Zenia Villa <zeniavilla@hotmail.com>
2017-04-13 20:09:26 -0700

    added hash with properties to .scrape_destination_page method

--- lib/top_travel_destinations/scraper.rb
+++ lib/top_travel_destinations/scraper.rb
@@ -20,6 +20,13 @@ class TopTravelDestinations::Scraper
     def self.scrape_destination_page(destination_url)
         page = Nokogiri::HTML(open(destination_url))
 
+        destination_info = {
+            :description => page.css("#taplc_expanding_read_more_box_0 .content").text.strip,
+            :attractions => page.css(".col.attractions li .name").collect {|attr| attr.text.strip},
+            :attractions_url => "https://www.tripadvisor.com#{page.css(".col.attractions .seeAllLink").attribute("href").value}"
+        }
 
+        destination_info[:attractions] << page.css(".col.attractions li .name").each {|attr| attr.text.strip},
     end
+
 end
